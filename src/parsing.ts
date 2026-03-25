99aac54fbbc6b29e9434cf30edc01939459368ff
MortalFlame21 <markgio2100@gmail.com>
2026-03-25 12:44:52 +1100

    update: parsing.ts. Improve parsing validation.

--- src/parsing.ts
+++ src/parsing.ts
@@ -1,24 +1,46 @@
+import type { Board, Rolls } from "./monopoly.js";
+import { board, rolls } from "./monopoly.js";
 import { readFileSync } from "node:fs";
 
-export function parseBoard(path: string) {}
-
-export function parseRolls(path: string) {
-  let buffer;
+function returnJson(path: string) {
   try {
-    buffer = JSON.parse(readFileSync(path, "utf-8"));
+    return JSON.parse(readFileSync(path, "utf-8"));
   } catch {
-    throw new Error(`Failed to <ROLLS>, ${path}, please check path.`);
+    throw new Error(
+      `Error:Failed to read JSON from ${path}, please check path.`,
+    );
   }
+}
 
-  // must be a non-empty array
-  if (!Array.isArray(buffer) || buffer.length === 0)
-    throw new Error(`<ROLLS>, ${path}, must be a non-empty JSON array.`);
+export function parseBoard(path: string): Board {
+  let buffer = returnJson(path);
 
-  // array must be positive integers
-  if (
-    !buffer.every((n) => typeof n === "number" && Number.isInteger(n) && n > 0)
-  )
-    throw new Error("Rolls must be an array of positive integers.");
+  // validate board
+  const res = board.safeParse(buffer);
+  if (!res.success)
+    throw new Error(
+      `ERROR: board: ${path}, failed to parse board JSON, please check format.`,
+    );
 
-  return buffer;
+  // go constraints
+  if (res.data[0].type !== "go")
+    throw new Error(`ERROR: ${path}, first tile must be of type "go".`);
+  if (res.data.filter((tile) => tile.type === "go").length !== 1)
+    throw new Error(
+      `ERROR: board: ${path}, must contain exactly one "go" tile.`,
+    );
+
+  return res.data;
+}
+
+export function parseRolls(path: string): Rolls {
+  let buffer = returnJson(path);
+
+  // validate rolls
+  const res = rolls.safeParse(buffer);
+  if (!res.success)
+    throw new Error(
+      `ERROR: rolls: ${path}, failed to parse rolls JSON, please check format.`,
+    );
+  return res.data;
 }
