67b2224fad639d0f8eefb7d98585503d4eca0a43
MortalFlame21 <markgio2100@gmail.com>
2026-03-25 13:42:16 +1100

    update: player.ts. Update player property data structure.

--- src/player.ts
+++ src/player.ts
@@ -1,29 +1,25 @@
 import type { Property } from "./monopoly.js";
 import { player } from "./monopoly.js";
+import { g_STARTING_MONEY } from "./game.js";
 
 // a class similar to Player type
 export class Player {
   name: string;
   position: number;
   money: number;
-  properties: Property[];
+  // property pos : property
+  properties: Map<number, Property>;
 
-  constructor(
-    name: string,
-    position: number,
-    money: number,
-    properties: Property[],
-  ) {
-    const parse = player.parse({ name, position, money, properties });
-
-    this.name = parse.name;
-    this.position = parse.position;
-    this.money = parse.money;
-    this.properties = parse.properties;
+  constructor(name: string, money: number = g_STARTING_MONEY) {
+    // start at 0, with default money, and no properties
+    this.name = name;
+    this.position = 0;
+    this.money = money;
+    this.properties = new Map<number, Property>();
   }
 
   // move player position, wraps around the board
-  public roll(dice: number, boardSize: number) {
+  public rollAndMove(dice: number, boardSize: number) {
     this.position = (this.position + dice) % boardSize;
   }
 
@@ -35,10 +31,15 @@ export class Player {
     return this.money < 0;
   }
 
+  public ownsProperty(property: Property) {
+    // simply check if the property pos is the as any of owned property pos
+    return this.properties.has(property.position);
+  }
+
   public buyProperty(property: Property) {
     if (this.canAfford(property.price)) {
       this.money -= property.price;
-      this.properties.push(property);
+      this.properties.set(property.position, property);
     }
   }
 
